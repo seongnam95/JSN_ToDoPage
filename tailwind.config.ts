54361b1c846195cb7e11bd42f8e3f5f2eba6bbf7
jsn0509 <jsn_@naver.com>
2025-02-18 06:19:05 +0900

    chore: TailwindCSS 테마 및 플러그인 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,33 @@
 import type { Config } from 'tailwindcss';
 import { fontFamily } from 'tailwindcss/defaultTheme';
+import { typoPlugin } from './src/styles/theme/typoPlugin';
 
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    spacing: {
+      px: '1px',
+      '0': '0',
+      '1.5': '0.6rem',
+      ...Object.fromEntries(
+        Array.from({ length: 50 }, (_, i) => [
+          i + 1,
+          `${((i + 1) * 4) / 10}rem`,
+        ])
+      ),
+    },
+    borderRadius: {
+      xs: '0.2rem',
+      sm: '0.4rem',
+      md: '0.8rem',
+      lg: '1.2rem',
+      full: '9999px',
+    },
+    screens: {
+      sm: '501px',
+      md: '768px',
+      lg: '1200px',
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
@@ -26,10 +50,15 @@ export default {
         DEFAULT: 'var(--border)',
         accent: 'var(--border-accent)',
       },
+      danger: {
+        DEFAULT: 'var(--danger)',
+        surface: 'var(--danger-surface)',
+      },
+      yellow: 'var(--yellow)',
     },
     fontFamily: {
       pretendard: ['var(--pretendard)', ...fontFamily.sans],
     },
   },
-  plugins: [],
+  plugins: [typoPlugin],
 } satisfies Config;
