af38f0c3010d0e68c30edad2d9ab4034133aa023
jsn0509 <jsn_@naver.com>
2025-02-18 07:44:20 +0900

    feat: Button 컴포넌트 구현

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -9,14 +9,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
-          'bg-primary text-primary-foreground shadow hover:bg-primary/90',
-        destructive:
-          'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
-        outline:
-          'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
+        default: 'bg-primary text-primary-foreground hover:bg-primary/90',
         ghost: 'hover:bg-surface',
-        link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
         default: 'h-9 px-4 py-2 typo-body-16',
