41fee1a8c439356146364a0995b534dce3953630
jsn0509 <jsn_@naver.com>
2025-02-18 20:00:51 +0900

    feat: 파라미터 보드 페이지 추가

---
+++ src/app/board/[id]/page.tsx
@@ -0,0 +1,20 @@
+import { notFound } from 'next/navigation';
+import { BoardHeader } from '@/components/BoardHeader';
+import { TodoView } from '@/components/TodoView';
+
+type BoardPageProps = {
+  params: { id?: string };
+};
+
+export default function BoardPage({ params: { id } }: BoardPageProps) {
+  if (!id) notFound();
+  return (
+    <div className='mx-auto flex h-screen max-w-[90rem] flex-col'>
+      <BoardHeader boardId={id} back />
+
+      <div className='flex-1 overflow-hidden px-5'>
+        <TodoView boardId={id} />
+      </div>
+    </div>
+  );
+}
