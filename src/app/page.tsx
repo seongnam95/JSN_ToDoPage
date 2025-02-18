c1e57a80c5a3737e8d408a731c7a521c18d56c47
jsn0509 <jsn_@naver.com>
2025-02-18 19:54:16 +0900

    refactor: 홈페이지 보드 리스트로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
+import { BoardList } from '@/components/BoardList';
 import { Header } from '@/components/Header';
-import { TodoView } from '@/components/TodoView';
 
 export default function Home() {
   return (
     <div className='mx-auto flex h-screen max-w-[90rem] flex-col'>
-      <Header />
+      <Header title='TODO!' />
 
-      <div className='flex-1 overflow-hidden'>
-        <TodoView />
+      <div className='flex-1 overflow-hidden px-5 py-6'>
+        <BoardList />
       </div>
     </div>
   );
