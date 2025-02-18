29b6150cb703ba8903d47a2ae9d162885a74d091
jsn0509 <jsn_@naver.com>
2025-02-18 20:00:36 +0900

    refactor: 메인페이지 보드 리스트로 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import { BoardList } from '@/components/BoardList';
 import { Header } from '@/components/Header';
+import { ScrollArea } from '@/components/ui/ScrollArea';
 
 export default function Home() {
   return (
     <div className='mx-auto flex h-screen max-w-[90rem] flex-col'>
       <Header title='TODO!' />
 
-      <div className='flex-1 overflow-hidden px-5 py-6'>
-        <BoardList />
+      <div className='flex-1 overflow-hidden'>
+        <ScrollArea className='h-full px-5'>
+          <BoardList className='pb-10 pt-6' />
+        </ScrollArea>
       </div>
     </div>
   );
