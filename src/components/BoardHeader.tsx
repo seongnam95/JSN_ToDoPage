f9e163491fbf67b92bf82fa65c7049c554abaa02
jsn0509 <jsn_@naver.com>
2025-02-18 20:14:01 +0900

    feat: 헤더 아이콘 추가

--- src/components/BoardHeader.tsx
+++ src/components/BoardHeader.tsx
@@ -2,6 +2,8 @@
 
 import { useTodo } from '@/hooks/useTodo';
 import { Header, HeaderProps } from '@/components/Header';
+import { JSX } from 'react';
+import { Package, Star } from 'lucide-react';
 
 interface BoardHeaderProps extends HeaderProps {
   boardId: string;
@@ -10,14 +12,23 @@ interface BoardHeaderProps extends HeaderProps {
 export function BoardHeader({ boardId, ...props }: BoardHeaderProps) {
   const { currentBoard } = useTodo({ boardId });
 
+  const fixedBoardIconMap: Record<string, JSX.Element> = {
+    all: <Package />,
+    starred: <Star />,
+  };
+
+  const renderIcon = fixedBoardIconMap[boardId];
+
   const renderTitle = (
-    <div className='flex items-center gap-3'>
+    <div className='flex items-center gap-2'>
       {currentBoard.color && (
         <div
-          className='size-4 rounded-sm'
+          className='h-4 w-1.5 rounded-xs'
           style={{ backgroundColor: currentBoard.color }}
         />
       )}
+
+      <div className='[&>svg]:size-4'>{renderIcon}</div>
 
       <h2 className='typo-title-16'>{currentBoard.name}</h2>
     </div>
