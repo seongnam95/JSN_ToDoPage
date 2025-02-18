89ac1001ba931ed2f8af348b1d5aff5e968da8a3
jsn0509 <jsn_@naver.com>
2025-02-18 20:19:34 +0900

    refactor: starred 분리

--- src/components/TodoView/AllTodoList.tsx
+++ src/components/TodoView/AllTodoList.tsx
@@ -3,8 +3,14 @@ import { ScrollArea } from '@/components/ui/ScrollArea';
 import { TodoList } from './TodoList';
 import { EmptyTodo } from './EmptyTodo';
 
-export function AllTodoList() {
-  const { boardGroups, isEmptyTodo } = useTodo();
+interface AllTodoListProps {
+  boardId: string;
+}
+
+export function AllTodoList({ boardId }: AllTodoListProps) {
+  const { boardGroups, isEmptyTodo } = useTodo({ boardId });
+
+  const groups = boardGroups.filter((group) => group.todos.length > 0);
 
   if (isEmptyTodo) return <EmptyTodo />;
 
@@ -12,22 +18,29 @@ export function AllTodoList() {
     <div className='flex h-full flex-col gap-4 py-6'>
       <div className='flex-1 overflow-hidden'>
         <ScrollArea className='h-full'>
-          {boardGroups.map((board) => (
-            <div key={board.id}>
-              <div className='flex items-center gap-3 px-6'>
-                <div
-                  className='h-4 w-1.5 rounded-xs'
-                  style={{ backgroundColor: board.color }}
+          {groups.map((board) => {
+            const isStarred = boardId === 'starred';
+            const todos = isStarred
+              ? board.todos.filter((todo) => todo.starred)
+              : board.todos;
+
+            return (
+              <div key={board.id}>
+                <div className='flex items-center gap-3 px-6'>
+                  <div
+                    className='h-4 w-1.5 rounded-xs'
+                    style={{ backgroundColor: board.color }}
+                  />
+                  <h2 className='typo-title-16'>{board.name}</h2>
+                </div>
+                <TodoList
+                  className='mt-1.5 px-5 pb-10'
+                  order={board.todoOrder}
+                  todos={todos}
                 />
-                <h2 className='typo-title-16'>{board.name}</h2>
               </div>
-              <TodoList
-                className='mt-1.5 px-5 pb-10'
-                order={board.todoOrder}
-                todos={board.todos}
-              />
-            </div>
-          ))}
+            );
+          })}
         </ScrollArea>
       </div>
     </div>
