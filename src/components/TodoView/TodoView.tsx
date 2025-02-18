1fc325c739115625667d2216ce4b9751a35c3946
jsn0509 <jsn_@naver.com>
2025-02-18 15:02:50 +0900

    refactor: 전체보기 컴포넌트 분리

--- src/components/TodoView/TodoView.tsx
+++ src/components/TodoView/TodoView.tsx
@@ -1,16 +1,19 @@
 'use client';
 
-import { useBoardStore } from '@/store/useBoardStore';
 import { useTodoStore } from '@/store/useTodoStore';
 import { useRef } from 'react';
+import { useTodo } from '@/hooks/useTodo';
+import { AllTodoList } from '@/components/TodoView/AllTodoList';
+import { ScrollArea } from '@/components/ui/ScrollArea';
 import { TodoInput } from './TodoInput';
 import { TodoList } from './TodoList';
 
 export function TodoView() {
   const scrollRef = useRef<HTMLDivElement>(null);
 
+  const { currentBoard, currentBoardTodos } = useTodo();
+
   const createTodo = useTodoStore((state) => state.createTodo);
-  const currentBoard = useBoardStore((state) => state.selectedBoard);
 
   const handleEnter = (text: string, starred: boolean) => {
     const boardId = ['all', 'starred'].includes(currentBoard.id)
@@ -25,12 +28,17 @@ export function TodoView() {
     scrollRef.current?.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
+  if (currentBoard.id === 'all') return <AllTodoList />;
   return (
-    <div className='flex h-full flex-col gap-4 px-5 py-6'>
-      <TodoInput onEnter={handleEnter} />
+    <div className='flex h-full flex-col gap-4 py-6'>
+      <div className='px-5'>
+        <TodoInput onEnter={handleEnter} />
+      </div>
 
       <div className='flex-1 overflow-hidden'>
-        <TodoList scrollRef={scrollRef} boardId={currentBoard.id} />
+        <ScrollArea ref={scrollRef} className='h-full'>
+          <TodoList className='mt-2 px-5 pb-10' todos={currentBoardTodos} />
+        </ScrollArea>
       </div>
     </div>
   );
