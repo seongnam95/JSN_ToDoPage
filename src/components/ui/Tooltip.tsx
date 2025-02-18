66eed0b6ccfb53ec7a0e256876e6d569d94a756c
jsn0509 <jsn_@naver.com>
2025-02-18 23:06:44 +0900

    refactor: Tooltip 내용 title -> content 변경

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -22,7 +22,7 @@ const TooltipContent = ({
       ref={ref}
       sideOffset={sideOffset}
       className={cn(
-        'bg-black/70 z-50 overflow-hidden rounded-md px-3 py-1.5 text-primary-foreground typo-body-14 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
+        'z-50 overflow-hidden rounded-md bg-black/70 px-3 py-1.5 text-primary-foreground typo-body-14 animate-in fade-in-0 zoom-in-95 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
         className
       )}
       {...props}
@@ -34,13 +34,13 @@ TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 const Tooltip = ({
   children,
   asChild,
-  title,
+  content,
 }: React.ComponentProps<typeof TooltipPrimitive.Content>) => {
   return (
     <TooltipProvider>
       <TooltipRoot>
         <TooltipTrigger asChild={asChild}>{children}</TooltipTrigger>
-        <TooltipContent>{title}</TooltipContent>
+        <TooltipContent>{content}</TooltipContent>
       </TooltipRoot>
     </TooltipProvider>
   );
